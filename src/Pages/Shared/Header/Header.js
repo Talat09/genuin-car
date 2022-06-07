e7857e6b24afd03f1d33eb8a3945a58c3f83b0d5
Talat09 <talat181008312@gmail.com>
2022-06-07 19:23:51 +0600

    added navbar top fixed

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -3,12 +3,14 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 import logo from '../../../images/logo.png'
 const Header = () => {
     return (
-        <>
+        <div class="fixed-top ">
             <Navbar bg="primary" variant="dark">
                 <Container>
                     <Navbar.Brand href="#home">
                         <img src={logo} height='30px' alt="" />
                     </Navbar.Brand>
+
+
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#features">Features</Nav.Link>
@@ -16,7 +18,7 @@ const Header = () => {
                     </Nav>
                 </Container>
             </Navbar>
-        </>
+        </div>
     );
 };
 
